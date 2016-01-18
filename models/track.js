10c702b7aa0f7d6574260a1386e54ee19365311a
oscaryhusse <oscarvb94@hotmail.com>
2016-01-18 23:02:54 +0100

    Update track.js

--- models/track.js
+++ models/track.js
@@ -11,39 +11,21 @@ track_id: {
 
 */
 
-var mongoose = require('mongoose'),  
-    Schema   = mongoose.Schema;
-
-var musicSchema = new Schema({  
-  name:     { type: String },
-  url:      { type: String },
-  namepic:  { type: String },
-  urlpic:   { type: String },
-});
-
 exports.tracks = {
 	1: {
 		name: 'Cute',
 		url: '/media/Cute.mp3'
-		namepic: 'dio'
-		urlpic: 'iyi'
 	},
 	2: {
 		name: 'Dubstep',
 		url: '/media/Dubstep.mp3'
-		namepic: 'dio'
-		urlpic: 'iyi'		
 	},
 	3: {
 		name: 'Epic',
 		url: '/media/Epic.mp3'
-		namepic: 'dio'
-		urlpic: 'iyi'		
 	},
 	4: {
 		name: 'Littleidea',
 		url: '/media/Littleidea.mp3'
-		namepic: 'dio'
-		urlpic: 'iyi'		
 	}
 };
