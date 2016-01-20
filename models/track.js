c8464d96e44dae35c9b2827021b98471046be160
oscaryhusse <oscarvb94@hotmail.com>
2016-01-21 00:24:08 +0100

    Update track.js

--- models/track.js
+++ models/track.js
@@ -23,21 +23,4 @@ var musicSchema = new Schema({
 
 module.exports = mongoose.model('Music', musicSchema);
 
-exports.tracks = {
-	1: {
-		name: 'Cute',
-		url: '/media/Cute.mp3'
-	},
-	2: {
-		name: 'Dubstep',
-		url: '/media/Dubstep.mp3'
-	},
-	3: {
-		name: 'Epic',
-		url: '/media/Epic.mp3'
-	},
-	4: {
-		name: 'Littleidea',
-		url: '/media/Littleidea.mp3'
-	}
 };
