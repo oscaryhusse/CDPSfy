98d6fd0488e5cfe9a8cc9b847d214b810997eac5
oscaryhusse <oscarvb94@hotmail.com>
2016-01-20 21:21:14 +0100

    Update track_controller.js

--- controllers/track_controller.js
+++ controllers/track_controller.js
@@ -1,216 +1,185 @@
-var fs = require('fs');
 var express = require('express');
+var fs = require('fs');
 var http = require('http');
-var querystring = require('querystring');
 var mongoose = require('mongoose');
 var needle = require('needle');
-var Tracks = mongoose.model('Track');
+var querystring = require('querystring');
+
+var info = '';
+var music = mongoose.model('Music');
 
 // Devuelve una lista de las canciones disponibles y sus metadatos
 exports.list = function (req, res) {
-        //Busca en la base de datos las canciones existentes para mostrarlas en una lista
-        Tracks.find(function(err, tracks) {
-            if(err) res.send(500, err.message);
-            res.render('tracks/index', {tracks: tracks});
-        });
+	music.find(function(err, tracks) {
+		if (err)
+			res.send(500, err.message);
+		res.render('tracks/index', {tracks: tracks});
+	});
 };
 
 // Devuelve la vista del formulario para subir una nueva canción
 exports.new = function (req, res) {
-	var texto = '';
-	res.render('tracks/new', {texto:texto});
+	res.render('tracks/new', {info:info});
 };
 
 // Devuelve la vista de reproducción de una canción.
 // El campo track.url contiene la url donde se encuentra el fichero de audio
-
 exports.show = function (req, res) {
-    
 	console.log(req.params);
-        Tracks.findOne({name: req.params.trackId}, function(err, track) {
-	    if(err) return res.send(500, err.message);
-            res.render('tracks/show', {track: track});
-        });
+	music.findOne({name: req.params.trackId}, function(err, track) {
+	if (err)
+		return res.send(500, err.message);
+	res.render('tracks/show', {track: track});
+	});
 };
 
-//Escribe una nueva canción en el registro de canciones.
-//Escribe en tracks.cdpsfy.es el fichero de audio contenido en req.files.track.buffer
-//Escribe en la base de datos la verdadera url generada al añadir el fichero en el servidor tracks.cdpsfy.es
+// Escribe una nueva canción en el registro de canciones.
+// Escribe en tracks.cdpsfy.es el fichero de audio contenido en req.files.track.buffer
+// Escribe en la base de data la verdadera url generada al añadir el fichero en el servidor tracks.cdpsfy.es
 exports.create = function (req, res) {
-	var texto = '';
-        var track = req.files.track;
-		//Si la cancion es undefined no se ha introducido nada
-		if(track!==undefined){
-			console.log('Nuevo fichero de audio. Datos: ', track);
-			var id = track.name.split('.')[0];
-			var name = track.originalname.split('.')[0];
-			var datos = track.buffer;
-			var original = track.originalname; 
-			var ext = track.extension;
-			//Si la extension no se corresponde con un fichero de audio, no hacemos nada
-			if(ext == 'mp3' || ext == 'ogg' || ext== 'wav'){
-				console.log(ext);
-				var image = req.files.image;
-				//Si la imagen no existe, ponemos una por defecto
-				if(image!==undefined){
-					console.log('Nueva portada. Datos: ', image);
-					var nameImg = image.originalname.split('.')[0];
-					var datosImg =  image.buffer;
-					var originalImg = image.originalname;
-					var ext1 = image.extension;
-					//Comprobamos la extensión de la imagen
-					if (ext1 == 'bmp' || ext1 == 'jpg' || ext1 == 'png' || ext1 == 'jpeg' || ext1 == 'gif'){
-						//archivos enviados en la petición post al servidor
-						var data = {
-						   image: {
-								buffer       : datosImg,
-								filename     : image.originalname,
-								content_type: image.mimetype
-							  },
-							  
-						   track:  {
-								buffer       : datos,
-								filename     : track.originalname,
-								content_type: track.mimetype
-							  }
-						}
-						// Esta url debe ser la correspondiente al nuevo fichero en tracks.cdpsfy.es
-						var url = 'http://tracks.cdpsfy.es/cancion/' + track.originalname;
-						var urlImg = 'http://tracks.cdpsfy.es/imagen/' + image.originalname;
-						Tracks.find({name: name}, function(err, tracks) {
-								if(tracks == ''){
-									// Escribe los metadatos de la nueva canción en el registro.
-									var new_track = new Tracks({
-										name: name,
-										url: url,
-										imgname: image.originalname,
-										urlImg: urlImg
-									 });
-									//Guardamos la canción en la Base de datos
-									new_track.save(function(err, new_track) {
-										if (err) {
-											console.log('Error al subir el audio: ' + err);
-										};
-									});
-									//Mandamos la petición POST al servidor para guardar la canción y la imagen
-									needle.post('http://tracks.cdpsfy.es', data, {multipart: true}, function optionalCallback(err, httpResponse, body) {
-										if (err) {
-										return console.error('upload failed:', err);
-										}
-										console.log('Upload successful!  Server responded with:', body);
-										res.redirect('/tracks');
-									});
-								} else {
-									texto = 'Ya existe una cancion con ese nombre';
-									res.render('tracks/new', {texto:texto});							
-								}
-					});
-					} else {
-						console.log('Introduzca una imagen. Extensiones soportadas: gif, bmp, jpg, png, jpeg');
-					}
+	var track = req.files.track;
+
+	if (track !== undefined) {
+		console.log('Nuevo tracks. Info: ', track);
+		var id = track.name.split('.')[0];
+		var name = track.originalname.split('.')[0];
+		var data = track.buffer;
+//		var original = track.originalname; 
+		var ext = track.extension;
+
+		if (ext !== 'mp3' || ext !== 'wav' || ext !== 'ogg') {
+			console.log('Please upload .mp3, .ogg or .wav tracks');
+			res.redirect('/tracks');
+		} else {
+			console.log(ext);
+			var cover = req.files.cover;
+
+			if (cover !== undefined) {
+				console.log('New cover. Info ', cover);
+//				var nameImg = cover.originalname.split('.')[0];
+				var datacover =  cover.buffer;
+//				var originalImg = cover.originalname;
+				var ext1 = cover.extension;
+
+				if (ext1 !== 'jpeg' || ext1 !== 'jpg' || ext1 !== 'png' || ext1 !== 'bmp' || ext1 !== 'gif') {
+					console.log('Please upload .jpeg (.jpg), .png, .bmp or .gif images');
 				} else {
 					var data = {
-						track:  {
-						    buffer       : datos,
-					    	filename     : track.originalname,
-					    	content_type: track.mimetype
+						cover: {
+							buffer		: datacover,
+							filename	: cover.originalname,
+							content_type: cover.mimetype
+						},
+						  
+						track: {
+							buffer		: data,
+							filename	: track.originalname,
+							content_type: track.mimetype
 						}
-	 				}
+					}
+
 					var url = 'http://tracks.cdpsfy.es/cancion/' + track.originalname;
-					var urlImg = 'http://tracks.cdpsfy.es/imagen/cover.jpg';
-					Tracks.find({name: name}, function(err, tracks) {
-						if(tracks == ''){
-							//Escribe los metadatos de la nueva canción en la base de datos.
-							var new_track = new Tracks({
+					var urlpic = 'http://tracks.cdpsfy.es/imagen/' + cover.originalname;
+
+					music.find({name: name}, function(err, tracks) {
+						if (tracks == '') {
+							var new_track = new music({
 								name: name,
 								url: url,
-								imgname: '',
-								urlImg: urlImg
+								namepic: cover.originalname,
+								urlpic: urlpic
 							});
-						 	new_track.save(function(err, new_track) {
+
+							new_track.save(function(err, new_track) {
 								if (err) {
-									  console.log('Error al subir el audio: ' + err);
+									console.log('Upload failed: ' + err);
 								};
 							});
-							//Mandamos la petición POST al servidor para guardar la canción
+
 							needle.post('http://tracks.cdpsfy.es', data, {multipart: true}, function optionalCallback(err, httpResponse, body) {
-						 		if (err) {
-									return console.error('upload failed:', err);
-									}
-									console.log('Upload successful!  Server responded with:', body);
-									res.redirect('/tracks');
+								if (err) 
+									return console.error('Upload failed:', err);
+								console.log('Upload successful. Server response:', body);
+								res.redirect('/tracks');
 							});
 						} else {
-								texto = 'Ya existe una cancion con ese nombre en la base de datos';
-								res.render('tracks/new', {texto:texto});	
+							info = 'There is already a track with that name';
+							res.render('tracks/new', {info:info});							
 						}
 					});
 				}
-			} else { 
-				console.log('Introduzca una cancion con la extension adecuada. Extensiones soportadas: mp3, ogg y wav');
-				//redirigimos al index
-				res.redirect('/tracks');
+			} else {
+				var data = {
+					track: {
+					    buffer      : data,
+				    	filename    : track.originalname,
+				    	content_type: track.mimetype
+					}
+ 				}
+
+				var url = 'http://tracks.cdpsfy.es/cancion/' + track.originalname;
+				var urlpic = 'http://tracks.cdpsfy.es/imagen/cover.jpg';
+				
+				music.find({name: name}, function(err, tracks) {
+					if (tracks == '') {
+						var new_track = new music({
+							name: name,
+							url: url,
+							namepic: '',
+							urlpic: urlpic
+						});
+
+					 	new_track.save(function(err, new_track) {
+							if (err)
+								console.log('Error al subir el audio: ' + err);
+						});
+
+						needle.post('http://tracks.cdpsfy.es', data, {multipart: true}, function optionalCallback(err, httpResponse, body) {
+					 		if (err)
+								return console.error('upload failed:', err);
+							console.log('Upload successful. Server response:', body);
+							res.redirect('/tracks');
+						});
+					} else {
+						info = 'There is already a track with that name';
+						res.render('tracks/new', {info:info});	
+					}
+				});
 			}
-		} else { 
-			console.log('Introduzca una canción');
-			res.redirect('/tracks');
 		}
+	} else { 
+		console.log('Introduzca una canción');
+		res.redirect('/tracks');
+	}
 };
 
-//Borra una canción (trackId) de la base de datos 
-//Eliminar en tracks.cdpsfy.es el fichero de audio correspondiente a trackId
+// Elimina en tracks.cdpsfy.es el fichero de audio correspondiente a trackId
 exports.destroy = function (req, res) {
-
-	// Aquí debe implementarse el borrado del fichero de audio indetificado por trackId en tracks.cdpsfy.es
-	
-	Tracks.findOne({name: req.params.trackId}, function(err, track) {
-		Tracks.find({imgname: track.imgname}, function(err, track1) {
+	music.findOne({name: req.params.trackId}, function(err, track) {
+		music.find({namepic: track.namepic}, function(err, track1) {
 			//Si el nombre de la imagen es '' es que usa la imagen por defecto, y por tanto no la borra del servidor
-			if (track.imgname !== '' && track1.length == 0){
-				needle.request('delete', 'http://tracks.cdpsfy.es/imagen/' + track.imgname, null, function(err, resp) {
-					if (err) {
-					return console.error('Delete failed:', err);
-					}
+			if (track.namepic !== '' && track1.length == 0){
+				needle.request('delete', 'http://tracks.cdpsfy.es/imagen/' + track.namepic, null, function(err, resp) {
+					if (err) 
+						return console.error('Delete failed:', err);
 					console.log('Delete successful!  Server responded with:', resp.body);
 				});
 			}
 		 });
-		//Borra la canción de la base de datos
-	    track.remove(function(err, track) {
-                if (err) {
-                        console.log('Error al borrar el audio: ' + err);
-                };
-            });
-        });
-	//Petición HTTP para borrar la canción del servidor nas
+
+		// Borra la canción de la base de datos
+		track.remove(function(err, track) {
+			if (err)
+				console.log('Error al borrar el audio: ' + err);
+		});
+	});
+
+	// Petición HTTP para borrar la canción del servidor nas
 	needle.request('delete', 'http://tracks.cdpsfy.es/cancion/' + req.params.trackId + '.mp3', null, function(err, resp) {
-	  if (err) {
-	    return console.error('Delete failed:', err);
-	  }
-	  console.log('Delete successful!  Server responded with:', resp.body);
+		if (err)
+			return console.error('Delete failed:', err);
+		console.log('Delete successful!  Server responded with:', resp.body);
 	});
 
 	res.redirect('/tracks');
-};
-
-exports.buscar = function (req, res) {
-	//Búsqueda introducida
-	var busqueda = req.query.palabra;
-	//Si está vacía salta warning
-	if(busqueda ==''){
-	    res.render('buscar/criterios');
-	} else {
-		//Si no la busca en la base de datos y las muestra
-		Tracks.find({name: busqueda}, function(err, tracks) {
-		    if(err) {
-				res.send(500, err.message);
-		    }
-			//Si la canción no existe salta warning
-		    if(tracks == ''){ 
-				res.render('buscar/resultado'); 
-		    } else {
-				res.render('tracks/index', {tracks: tracks});
-			}
-		});
-	}
 };
